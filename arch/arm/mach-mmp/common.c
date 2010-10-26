f1523922aca7df86e1c65ce389d50f16c677d39e
Ofer Zaarur <ofer.zaarur@marvell.com>
2010-10-26 09:57:44 -0400

    pxa168: add va_to_pa, upa_to_va functions
    Signed-off-by: Ofer Zaarur <ofer.zaarur@marvell.com>
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- arch/arm/mach-mmp/common.c
+++ arch/arm/mach-mmp/common.c
@@ -11,11 +11,13 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/kallsyms.h>
 #include <linux/sched.h>
 #include <linux/interrupt.h>
 
 #include <asm/page.h>
 #include <asm/mach/map.h>
+#include <asm/cacheflush.h>
 #include <mach/addr-map.h>
 #include <mach/regs-pcie.h>
 
@@ -64,3 +66,57 @@ void get_RIPC(void)
 	}
 }
 
+/*EXPORT_SYMBOL(dmac_flush_range);
+EXPORT_SYMBOL(dmac_clean_range);*/
+EXPORT_SYMBOL(kallsyms_lookup_name);
+static unsigned long uva_to_pa(unsigned long addr, struct page **page)
+{
+	unsigned long ret = 0UL;
+	pgd_t *pgd;
+	pud_t *pud;
+	pmd_t *pmd;
+	pte_t *pte;
+
+	pgd = pgd_offset(current->mm, addr);
+	if (!pgd_none(*pgd)) {
+		pud = pud_offset(pgd, addr);
+		if (!pud_none(*pud)) {
+			pmd = pmd_offset(pud, addr);
+			if (!pmd_none(*pmd)) {
+				pte = pte_offset_map(pmd, addr);
+				if (!pte_none(*pte) && pte_present(*pte)) {
+					(*page) = pte_page(*pte);
+					ret = page_to_phys(*page);
+					ret |= (addr & (PAGE_SIZE-1));
+				}
+			}
+		}
+	}
+	return ret;
+}
+EXPORT_SYMBOL(uva_to_pa);
+
+unsigned long va_to_pa(unsigned long user_addr, unsigned int size)
+{
+	unsigned long  paddr, paddr_tmp;
+	unsigned long  size_tmp = 0;
+	struct page *page = NULL;
+	int page_num = PAGE_ALIGN(size) / PAGE_SIZE;
+	unsigned int vaddr = PAGE_ALIGN(user_addr);
+	int i = 0;
+
+	if (!vaddr)
+		return 0;
+
+	paddr = uva_to_pa(vaddr, &page);
+
+	for (i = 0; i < page_num; i++) {
+		paddr_tmp = uva_to_pa(vaddr, &page);
+		if ((paddr_tmp - paddr) != size_tmp)
+			return 0;
+		vaddr += PAGE_SIZE;
+		size_tmp += PAGE_SIZE;
+	}
+	return paddr;
+}
+EXPORT_SYMBOL(va_to_pa);
