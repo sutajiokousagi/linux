b15f4e81e6527b39eca04b7d73d7e5dea84cdb92
Sean Cross <xobs@kosagi.com>
2012-02-21 17:51:59 +0800

    stmpe: Get compiling

--- drivers/mfd/stmpe.c
+++ drivers/mfd/stmpe.c
@@ -8,7 +8,7 @@
  */
 
 #include <linux/gpio.h>
-#include <linux/export.h>
+#include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
--- drivers/mfd/stmpe.h
+++ drivers/mfd/stmpe.h
@@ -11,7 +11,6 @@
 #include <linux/device.h>
 #include <linux/mfd/core.h>
 #include <linux/mfd/stmpe.h>
-#include <linux/printk.h>
 #include <linux/types.h>
 
 extern const struct dev_pm_ops stmpe_dev_pm_ops;
